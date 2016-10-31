f182c1bf7d12e43c279d1b0981c5e2bda52aa372
wangyu <wangyu@273.cn>
2016-10-31 17:24:29 +0800

    use pcall to catch exception

--- app/post.lua
+++ app/post.lua
@@ -1,5 +1,16 @@
-return {
-    ["get"] = function()
-        ngx.say("you get posts")
-    end,
-}
+
+local Post = {}
+
+Post.index = function(data)
+    ngx.say("I'm your father")
+end
+
+Post.get = function()
+    ngx.say("you get posts")
+end
+
+Post.get_latest_posts = function()
+    ngx.say("you get latest posts")
+end
+
+return Post
--- init.lua
+++ init.lua
@@ -5,11 +5,21 @@ local data   = ngx.req.get_uri_args()
 
 -- router
 local r = require("core.router"):new()
+local pcall  = pcall
 r:any("/:app/:controller/:action/", function(params, method)
     local path = params.app .. "." .. params.controller
-    ngx.say(path .. "." .. params.action)
-    local controller = require(path)
-    controller[params.action]()
+    local action = params.action
+
+    -- run action
+    local ok, err = pcall(function()
+        local controller = require(path)
+        controller[action]()
+    end)
+
+    if err then
+        ngx.status = 404
+        ngx.say(err)
+    end
 end)
 local ok, err = r:execute(method, uri, data)
 
